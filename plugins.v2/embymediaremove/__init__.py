322295f1309bbaee2f8dfb75193cb9e1a72af065
dzplus <45281375+dzplus@users.noreply.github.com>
2025-02-05 19:40:43 +0800

    摸索一下

--- plugins.v2/embymediaremove/__init__.py
+++ plugins.v2/embymediaremove/__init__.py
@@ -1,14 +1,13 @@
 import logging
-import time
+
 from typing import Any, List, Dict, Tuple, Optional
 
 from app.core.event import eventmanager, Event
 from app.helper.mediaserver import MediaServerHelper
 from app.log import logger
 from app.plugins import _PluginBase
-from app.schemas import WebhookEventInfo, ServiceInfo
 from app.schemas.types import EventType, MediaType, MediaImageType, NotificationType
-from app.utils.web import WebUtils
+
 
 
 class EmbyMediaRemove(_PluginBase):
@@ -19,7 +18,7 @@ class EmbyMediaRemove(_PluginBase):
     # 插件图标
     plugin_icon = "mediaplay.png"
     # 插件版本
-    plugin_version = "0.3"
+    plugin_version = "0.4"
     # 插件作者
     plugin_author = "dzplus"
     # 作者主页
@@ -27,94 +26,35 @@ class EmbyMediaRemove(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "embymediaremove_"
     # 加载顺序
-    plugin_order = 100
+    plugin_order = 14
     # 可使用的用户级别
     auth_level = 1
 
     # 私有属性
     mediaserver_helper = None
-    _enabled = False
+    _enabled = True
     _add_play_link = False
     _mediaservers = None
     _types = []
     _webhook_msg_keys = {}
 
-    # 拼装消息内容
-    _webhook_actions = {
-        "library.new": "新入库",
-        "system.webhooktest": "测试",
-        "playback.start": "开始播放",
-        "playback.stop": "停止播放",
-        "user.authenticated": "登录成功",
-        "user.authenticationfailed": "登录失败",
-        "media.play": "开始播放",
-        "media.stop": "停止播放",
-        "PlaybackStart": "开始播放",
-        "PlaybackStop": "停止播放",
-        "item.rate": "标记了",
-        "library.deleted": "删除媒体"
-    }
-    _webhook_images = {
-        "emby": "https://emby.media/notificationicon.png",
-        "plex": "https://www.plex.tv/wp-content/uploads/2022/04/new-logo-process-lines-gray.png",
-        "jellyfin": "https://play-lh.googleusercontent.com/SCsUK3hCCRqkJbmLDctNYCfehLxsS4ggD1ZPHIFrrAN1Tn9yhjmGMPep2D9lMaaa9eQi"
-    }
-
+    #初始化插件
     def init_plugin(self, config: dict = None):
         self.mediaserver_helper = MediaServerHelper()
         if config:
             self._enabled = config.get("enabled")
-            self._types = config.get("types") or []
-            self._mediaservers = config.get("mediaservers") or []
-            self._add_play_link = config.get("add_play_link", False)
         logger.info("init_plugin invoke")
 
-    def service_infos(self, type_filter: Optional[str] = None) -> Optional[Dict[str, ServiceInfo]]:
-        """
-        服务信息
-        """
-        logger.info("service_infos invoke")
-        if not self._mediaservers:
-            logger.info("尚未配置媒体服务器，请检查配置")
-            return None
-        
-        services = self.mediaserver_helper.get_services(type_filter=type_filter, name_filters=self._mediaservers)
-        if not services:
-            logger.info("获取媒体服务器实例失败，请检查配置")
-            return None
-        logger.info(services)
-        active_services = {}
-        for service_name, service_info in services.items():
-            logger.info(service_name)
-            if service_info.instance.is_inactive():
-                logger.info(f"媒体服务器 {service_name} 未连接，请检查配置")
-            else:
-                active_services[service_name] = service_info
-
-        if not active_services:
-            return None
-        logger.info(active_services)
-        return active_services
-
-    def service_info(self, name: str) -> Optional[ServiceInfo]:
-        """
-        服务信息
-        """
-        logger.info("service_info invoke")
-        service_infos = self.service_infos() or {}
-        return service_infos.get(name)
-
-    def get_state(self) -> bool:
-        logger.info("get_state " + self._enabled)
-        return self._enabled
-
+    #发送事件
     @staticmethod
     def get_command() -> List[Dict[str, Any]]:
         pass
 
+    #从此插件暴露接口
     def get_api(self) -> List[Dict[str, Any]]:
         pass
 
+    #设定插件外观
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         logger.info("get_form invoke")
         """
@@ -172,155 +112,69 @@ class EmbyMediaRemove(_PluginBase):
             "types": []
         }
 
-    def get_page(self) -> List[dict]:
-        pass
-
     @eventmanager.register(EventType.PluginReload)
     def pluginReload(self, event: Event):
         logger.info("pluginReload invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.PluginAction)
-    def PluginAction(self, event: Event):
+    def pluginAction(self, event: Event):
         logger.info("PluginAction invoke")
-        logger.info(event.action)
+        logger.info(event)
         if not event or event.get("action") != "embyremove":
             return
 
     @eventmanager.register(EventType.SiteDeleted)
-    def SiteDeleted(self, event: Event):
+    def siteDeleted(self, event: Event):
         logger.info("SiteDeleted invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.DownloadAdded)
-    def DownloadAdded(self, event: Event):
+    def downloadAdded(self, event: Event):
         logger.info("DownloadAdded invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.HistoryDeleted)
-    def HistoryDeleted(self, event: Event):
+    def historyDeleted(self, event: Event):
         logger.info("HistoryDeleted invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.NoticeMessage)
-    def NoticeMessage(self, event: Event):
+    def noticeMessage(self, event: Event):
         logger.info("NoticeMessage invoke")
-        logger.info(event.event_data)
+        logger.info(event)
+        logger.info("NoticeMessage invoke down")
+
+    @eventmanager.register(EventType.TransferComplete)
+    def transferComplete(self, event: Event):
+        logger.info("transferComplete invoke")
+        logger.info(event)
 
     @eventmanager.register(EventType.SubscribeAdded)
-    def SubscribeAdded(self, event: Event):
+    def subscribeAdded(self, event: Event):
         logger.info("SubscribeAdded invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.SubscribeComplete)
-    def SubscribeComplete(self, event: Event):
+    def subscribeComplete(self, event: Event):
         logger.info("SubscribeComplete invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.SystemError)
-    def SystemError(self, event: Event):
+    def systemError(self, event: Event):
         logger.info("SystemError invoke")
-        logger.info(event.event_data)
-
+        logger.info(event)
 
     @eventmanager.register(EventType.SiteUpdated)
     def SiteUpdated(self, event: Event):
         logger.info("SiteUpdated invoke")
-        logger.info(event.event_data)
-
-    @eventmanager.register(EventType.PluginReload)
-    def pluginReload(self, event: Event):
-        logger.info("pluginReload invoke")
-        logger.info(event.event_data)
+        logger.info(event)
 
     @eventmanager.register(EventType.WebhookMessage)
     def send(self, event: Event):
         logger.info("send invoke")
-        """
-        发送通知消息
-        """
         logger.info(event)
-        if not self._enabled:
-            return
-
-        event_info: WebhookEventInfo = event.event_data
-        if not event_info:
-            logger.info("not event_info")
-            return
-
-        # 不在支持范围不处理
-        if not self._webhook_actions.get(event_info.event):
-            logger.info("not self._webhook_actions.get(event_info.event)")
-            return
-
-        # 不在选中范围不处理
-        msgflag = False
-        for _type in self._types:
-            if event_info.event in _type.split("|"):
-                msgflag = True
-                break
-        if not msgflag:
-            logger.info(f"未开启 {event_info.event} 类型的消息通知")
-            return
-
-        if not self.service_infos():
-            logger.info(f"未开启任一媒体服务器的消息通知")
-            return
-
-        if event_info.server_name and not self.service_info(name=event_info.server_name):
-            logger.info(f"未开启媒体服务器 {event_info.server_name} 的消息通知")
-            return
-
-        if event_info.channel and not self.service_infos(type_filter=event_info.channel):
-            logger.info(f"未开启媒体服务器类型 {event_info.channel} 的消息通知")
-            return
-
-        # 消息标题
-        if event_info.item_type in ["TV", "SHOW"]:
-            message_title = f"{self._webhook_actions.get(event_info.event)}剧集 {event_info.item_name}"
-        elif event_info.item_type == "MOV":
-            message_title = f"{self._webhook_actions.get(event_info.event)}电影 {event_info.item_name}"
-        elif event_info.item_type == "AUD":
-            message_title = f"{self._webhook_actions.get(event_info.event)}有声书 {event_info.item_name}"
-        else:
-            message_title = f"{self._webhook_actions.get(event_info.event)}"
-        logging.info(message_title)
-        # 消息内容
-        message_texts = []
-        if event_info.user_name:
-            message_texts.append(f"用户：{event_info.user_name}")
-        if event_info.device_name:
-            message_texts.append(f"设备：{event_info.client} {event_info.device_name}")
-        if event_info.ip:
-            message_texts.append(f"IP地址：{event_info.ip} {WebUtils.get_location(event_info.ip)}")
-        if event_info.percentage:
-            percentage = round(float(event_info.percentage), 2)
-            message_texts.append(f"进度：{percentage}%")
-        if event_info.overview:
-            message_texts.append(f"剧情：{event_info.overview}")
-        message_texts.append(f"时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
-
-        # 消息内容
-        message_content = "\n".join(message_texts)
-        logging.info(message_content)
-
-    def __add_element(self, key, duration=600):
-        expiration_time = time.time() + duration
-        # 如果元素已经存在，更新其过期时间
-        self._webhook_msg_keys[key] = expiration_time
-
-    def __remove_element(self, key):
-        self._webhook_msg_keys = {k: v for k, v in self._webhook_msg_keys.items() if k != key}
-
-    def __get_elements(self):
-        current_time = time.time()
-        # 过滤掉过期的元素
-        self._webhook_msg_keys = {k: v for k, v in self._webhook_msg_keys.items() if v > current_time}
-        return list(self._webhook_msg_keys.keys())
 
     def stop_service(self):
-        """
-        退出插件
-        """
-        logging.info("退出插件")
+        logger.info("stop_service")
         pass
